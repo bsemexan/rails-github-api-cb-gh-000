c978811d606c8295071628e27a8936c954b2b58d
Berry  Semexan <bsemexan@gmail.com>
2018-07-29 06:09:35 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,5 @@ class SessionsController < ApplicationController
     user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     user_json = JSON.parse(user_response.body)
     session[:username] = user_json["login"]
-
-    redirect_to '/'
   end
 end
